71794ea1f6a5de68d0cb91ebb196201b445729fb
adieb.co.id@gmail.com <rahasia12345>
2017-06-07 12:02:30 +0700

    Initial Commit

---
+++ app/src/main/java/xir5n07/remidiuts/telkomschid/remednavigation/MainActivity.java
@@ -0,0 +1,15 @@
+package xir5n07.remidiuts.telkomschid.remednavigation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
