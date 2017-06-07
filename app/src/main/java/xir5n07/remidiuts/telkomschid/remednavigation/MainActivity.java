49f32f953615522941f92c091870001447c70aca
adieb.co.id@gmail.com <rahasia12345>
2017-06-07 13:45:08 +0700

    Final Commit

--- app/src/main/java/xir5n07/remidiuts/telkomschid/remednavigation/MainActivity.java
+++ app/src/main/java/xir5n07/remidiuts/telkomschid/remednavigation/MainActivity.java
@@ -1,7 +1,10 @@
 package xir5n07.remidiuts.telkomschid.remednavigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Button bt1 = (Button) findViewById(R.id.button1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SwipeViews.class));
+            }
+        });
+        Button bt2 = (Button) findViewById(R.id.button2);
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Tabs.class));
+            }
+        });
+        Button bt3 = (Button) findViewById(R.id.button3);
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ActionBarSpinner.class));
+            }
+        });
+        Button bt4 = (Button) findViewById(R.id.button4);
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NavigationDrawer.class));
+            }
+        });
     }
 }
